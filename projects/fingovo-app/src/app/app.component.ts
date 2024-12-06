26deeeba71e5a66385a77e404f5a4a78bbd048da
Micha Kolanecki <michalkolanecki@gmail.com>
2024-12-06 06:47:25 +0100

    Install and setup PWA dependencies

--- projects/fingovo-app/src/app/app.component.ts
+++ projects/fingovo-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly #swUpdate: SwUpdate = inject(SwUpdate);
+
+  ngOnInit() {
+    //
+    // TEMPORARY, SHOULD BE FIXED, ITS RELOAD PERMANENTLY
+    //
+    // if (this.#swUpdate.isEnabled) {
+    //   this.#swUpdate.versionUpdates.subscribe(() => {
+    //     if (confirm('New version available. Load New Version?')) {
+    //       window.location.reload();
+    //     }
+    //   });
+    // }
+  }
+}
--- projects/fingovo-app/src/app/app.config.ts
+++ projects/fingovo-app/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import {
   PreloadAllModules,
   provideRouter,
@@ -12,6 +16,7 @@ import {
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,5 +25,11 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideIonicAngular(),
     provideHttpClient(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
