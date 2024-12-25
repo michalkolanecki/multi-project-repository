a178fe1aa849fbed5bdc70622227b6be026c669a
Micha Kolanecki <michalkolanecki@gmail.com>
2024-12-25 19:24:58 +0100

    Create icon, button, navigation-bar, navigation-tabs components

---
+++ projects/ui-shared-elements/src/lib/components/icon/icon.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "lib-icon",
+    templateUrl: "./icon.component.html",
+    styleUrls: ["./icon.component.scss"],
+    standalone: true,
+})
+export class IconComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
--- projects/ui-shared-elements/src/public-api.ts
+++ projects/ui-shared-elements/src/public-api.ts
@@ -1,3 +1,16 @@
 /*
  * Public API Surface of ui-shared-elements
  */
+// COMPONENTS
+export * from "./lib/components/icon/icon.component";
+export * from "./lib/components/button/button.component";
+export * from "./lib/components/navigation-bar/navigation-bar.component";
+export * from "./lib/components/navigation-tabs/navigation-tabs.component";
+
+// DIRECTIVES
+
+// HELPERS
+
+// PIPES
+
+// SCREENS
